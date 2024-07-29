8d5bb32c29b9e408ca58ce6b79e7e381f95c8f69
NinaMadu <136308860+NinaMadu@users.noreply.github.com>
2024-07-29 18:43:26 +0530

    Create new header and side bar
    Create new header and side bar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,88 +1,51 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import { FaSearch, FaBell, FaUserCircle, FaHeart } from 'react-icons/fa';
-import { GiHamburgerMenu } from 'react-icons/gi';
-import Sidebar from './Sidebar';
+import { FaSearch } from 'react-icons/fa';
+import { Link, useNavigate } from 'react-router-dom';
 import logo from '../assets/logo.png';
 
-function Header() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  const location = useLocation();
 
-  useEffect(() => {
-    if (location.pathname === '/createcourse') {
-      setIsAuthenticated(true);
-    } else {
-      setIsAuthenticated(false);
-    }
-  }, [location.pathname]);
-
-  const handleSidebarToggle = () => {
-    setIsSidebarOpen(!isSidebarOpen);
-  };
-
+export default function Header() {
+  
   return (
-    <>
-      <header className="bg-custom-pink w-full fixed top-0 z-40">
-        <div className="flex flex-wrap justify-between items-center mx-auto sm:px-8 py-2">
-          {isAuthenticated && (
-            <div className="order-1 sm:order-none">
-              <button
-                onClick={handleSidebarToggle}
-                className="inline-flex items-center justify-center rounded-md p-2 text-gray-500 hover:bg-gray-400 hover:text-white focus:outline-none focus:ring-1 focus:ring-inset focus:ring-slate-200"
-              >
-                <GiHamburgerMenu className="block h-6 w-6" aria-hidden="true" />
-              </button>
-            </div>
-          )}
+    <header className='bg-custom-pink shadow-md'>
+      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+        <Link to='/'>
+        <img src={logo} alt="Logo" className="h-8 sm:h-16" />
 
-          <Link to="/" className="flex items-center order-2 sm:order-none">
-            <img src={logo} alt="Logo" className="h-8 sm:h-16" />
+        </Link>
+        <form
+       
+          className='bg-white p-3 rounded-lg flex items-center'
+        >
+          <input
+            type='text'
+            placeholder='Search...'
+            className='bg-transparent focus:outline-none w-24 sm:w-64'
+          
+           
+          />
+          <button>
+            <FaSearch className='text-slate-600' />
+          </button>
+        </form>
+        <ul className='flex gap-8 text-[#2B3090] font-medium '>
+          <Link to='/'>
+            <li className='hidden sm:inline hover:text-custom-red transform hover:scale-110 transition-transform duration-300 '>
+              Home
+            </li>
           </Link>
-
-          <form className="bg-white py-1 px-4 sm:px-8 rounded-xl flex items-center w-full sm:w-auto shadow-md hover:shadow-lg transition-shadow duration-300 mt-4 sm:mt-0 order-3 sm:order-none">
-  <input
-    type="text"
-    placeholder="Search..."
-    className="bg-transparent focus:outline-none w-full sm:w-[24rem] md:w-[20rem] lg:w-[30rem] p-2 text-sm sm:text-base border border-transparent transition-colors duration-300"
-  />
-  <button
-    type="submit"
-    className="text-slate-500 ml-2 p-2 hover:text-slate-700 flex items-center justify-center bg-blue-100 hover:bg-blue-200 rounded-full transition-colors duration-300"
-  >
-    <FaSearch />
-  </button>
-</form>
-
-          <ul className="sm:mt-4 sm:ml-56 sm:mb-4 flex flex-wrap gap-4 sm:gap-12 items-center w-full sm:w-auto mt-4 sm:mt-0 order-4 sm:order-none justify-end">
-            <li>
-              <Link
-                to="/"
-                className="text-slate-700 font-semibold text-sm sm:text-base hover:text-custom-red transform hover:scale-110 transition-transform duration-300"
-              >
-                Home
-              </Link>
+          <Link to='/courses'>
+            <li className='hidden sm:inline hover:text-custom-red transform hover:scale-110 transition-transform duration-300' >
+              Courses
             </li>
-            <li>
-              <Link
-                to="/courses"
-                className="text-slate-700 font-semibold text-sm sm:text-base hover:text-custom-red transform hover:scale-110 transition-transform duration-300"
-              >
-                Courses
-              </Link>
+          </Link>
+          <Link to='/about'>
+            <li className='hidden sm:inline hover:text-custom-red transform hover:scale-110 transition-transform duration-300 '>
+              About
             </li>
-            <li>
-              <Link
-                to="/about"
-                className="text-slate-700 font-semibold text-sm sm:text-base hover:text-custom-red transform hover:scale-110 transition-transform duration-300"
-              >
-                About
-              </Link>
-            </li>
-            {!isAuthenticated ? (
-              <li>
-                <Link to="/sign-up">
+          </Link>
+    
+          
+        <Link to="/sign-up">
                   <span
                     className="text-white font-semibold border py-2 px-4 sm:py-2 sm:px-6 rounded-xl hover:opacity-90"
                     style={{
@@ -91,28 +54,10 @@ function Header() {
                   >
                     Get Started
                   </span>
-                </Link>
-              </li>
-            ) : (
-              <>
-                <li>
-                  <FaHeart className="text-slate-500 h-6 w-6" />
-                </li>
-                <li>
-                  <FaBell className="text-slate-500 h-6 w-6" />
-                </li>
-                <li>
-                  <FaUserCircle className="text-slate-500 h-6 w-6" />
-                </li>
-              </>
-            )}
-          </ul>
-        </div>
-      </header>
+        </Link>
+        </ul>
 
-      {isAuthenticated && <Sidebar isOpen={isSidebarOpen} />}
-    </>
+      </div>
+    </header>
   );
 }
-
-export default Header;
--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,90 +1,138 @@
-import React from 'react';
-import {
-  
-  MdOutlineIntegrationInstructions,
-  MdOutlineMoreHoriz,
-  MdOutlineSettings,
-  MdOutlineLogout,
-  MdDelete,
-} from 'react-icons/md';
-import { CgBell, CgHeart, CgHome, CgProfile } from 'react-icons/cg';
-import {  FaRegComments } from 'react-icons/fa';
-import {AiOutlineQuestionCircle} from 'react-icons/ai';
-import { BiMessageSquareDots } from 'react-icons/bi';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FaHeart, FaBell, FaUserCircle, FaSearch } from 'react-icons/fa';
+import logo from '../assets/logo.png';
+import { MdOutlineSettings, MdOutlineLogout, MdDelete } from 'react-icons/md';
+import { CgBell, CgHeart, CgHome, CgMenu, CgProfile } from 'react-icons/cg';
+import { FaRegComments } from 'react-icons/fa';
+import { AiOutlineQuestionCircle } from 'react-icons/ai';
 
-function Sidebar({ isOpen }) {
-  if (!isOpen) return null; //FDBDBD //DC6F6F
+export default function Sidebar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
-    <div className="fixed inset-0 z-30 flex">
-      <div className="fixed top-0 left-0 w-56 h-full bg-custom-pink  z-30">
-        <div className="flex flex-col justify-start items-center">
-          <div className="my-4 mt-36 border-b border-black pb-4 w-full">
-            
-          <div className="flex mb-2 justify-start items-center gap-4 pl-5 p-2 rounded-md group cursor-pointer hover:bg-[#DC6F6F] hover:text-white transition-colors duration-300">
-                    <CgProfile className="text-2xl text-gray-600 group-hover:text-white" />
-                    <h3 className="text-base text-gray-800 group-hover:text-white font-semibold">
-                      Profile
-                    </h3>
-                  </div>
-            <div className="flex mb-2 justify-start items-center gap-4 pl-5 p-2 rounded-md group cursor-pointer hover:bg-[#DC6F6F] hover:text-white transition-colors duration-300">
-              <CgHome className="text-2xl text-gray-600 group-hover:text-white" />
-              <h3 className="text-base text-gray-800 group-hover:text-white font-semibold">
+    <>
+      <header className='bg-custom-pink shadow-md'>
+        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+        <button
+            onClick={toggleSidebar}
+            className=""
+          >
+            <CgMenu className="text-2xl text-gray-600 group-hover:text-white" />
+          </button>
+          <Link to='/'>
+            <img src={logo} alt="Logo" className="h-8 sm:h-16" />
+          </Link>
+          <form className='bg-white p-3 rounded-lg flex items-center'>
+            <input
+              type='text'
+              placeholder='Search...'
+              className='bg-transparent focus:outline-none w-24 sm:w-64'
+            />
+            <button>
+              <FaSearch className='text-slate-600' />
+            </button>
+          </form>
+          <ul className='flex gap-4 text-[#2B3090] font-medium md:gap-12 '>
+            <Link to='/'>
+              <li className='hidden sm:inline hover:text-custom-red transform hover:scale-110 transition-transform duration-300'>
                 Home
-              </h3>
+              </li>
+            </Link>
+            <Link to='/courses'>
+              <li className='hidden sm:inline hover:text-custom-red transform hover:scale-110 transition-transform duration-300'>
+                Courses
+              </li>
+            </Link>
+            <Link to='/about'>
+              <li className='hidden sm:inline hover:text-custom-red transform hover:scale-110 transition-transform duration-300'>
+                About
+              </li>
+            </Link>
+            <li>
+              <FaHeart className="text-slate-500 h-6 w-6" />
+            </li>
+            <li>
+              <FaBell className="text-slate-500 h-6 w-6" />
+            </li>
+            <li>
+              <FaUserCircle className="text-slate-500 h-6 w-6" />
+            </li>
+          </ul>
+          
+        </div>
+      </header>
+      
+      {isOpen && (
+        <div className="fixed left-0 w-56 h-full bg-custom-pink z-50">
+          <div className="flex flex-col justify-start items-center">
+            <div className="my-4 mt-12 border-b border-black pb-4 w-full">
+              <div className="flex mb-2 justify-start items-center gap-4 pl-5 rounded-md group cursor-pointer hover:bg-[#DC6F6F] hover:text-white transition-colors duration-300 p-2">
+                <CgProfile className="text-2xl text-gray-600 group-hover:text-white " />
+                <h3 className="text-base text-gray-800 group-hover:text-white font-semibold ">
+                  Profile
+                </h3>
+              </div>
+              <div className="flex mb-2 justify-start items-center gap-4 pl-5 p-2 rounded-md group cursor-pointer hover:bg-[#DC6F6F] hover:text-white transition-colors duration-300">
+                <CgHome className="text-2xl text-gray-600 group-hover:text-white" />
+                <h3 className="text-base text-gray-800 group-hover:text-white font-semibold">
+                  Home
+                </h3>
+              </div>
+              <div className="flex mb-2 justify-start items-center gap-4 pl-5 p-2 rounded-md group cursor-pointer hover:bg-[#DC6F6F] hover:text-white transition-colors duration-300">
+                <CgBell className="text-2xl text-gray-600 group-hover:text-white" />
+                <h3 className="text-base text-gray-800 group-hover:text-white font-semibold">
+                  Notifications
+                </h3>
+              </div>
+              <div className="flex mb-2 justify-start items-center gap-4 pl-5 p-2 rounded-md group cursor-pointer hover:bg-[#DC6F6F] hover:text-white transition-colors duration-300">
+                <FaRegComments className="text-2xl text-gray-600 group-hover:text-white" />
+                <h3 className="text-base text-gray-800 group-hover:text-white font-semibold">
+                  Discussion
+                </h3>
+              </div>
+              <div className="flex mb-2 justify-start items-center gap-4 pl-5 p-2 rounded-md group cursor-pointer hover:bg-[#DC6F6F] hover:text-white transition-colors duration-300">
+                <CgHeart className="text-2xl text-gray-600 group-hover:text-white" />
+                <h3 className="text-base text-gray-800 group-hover:text-white font-semibold">
+                  Favourites
+                </h3>
+              </div>
+              <div className="flex mb-2 justify-start items-center gap-4 pl-5 p-2 rounded-md group cursor-pointer hover:bg-[#DC6F6F] hover:text-white transition-colors duration-300">
+                <AiOutlineQuestionCircle className="text-2xl text-gray-600 group-hover:text-white" />
+                <h3 className="text-base text-gray-800 group-hover:text-white font-semibold">
+                  Help and Support
+                </h3>
+              </div>
             </div>
-            <div className="flex mb-2 justify-start items-center gap-4 pl-5 p-2 rounded-md group cursor-pointer hover:bg-[#DC6F6F] hover:text-white transition-colors duration-300">
-              <CgBell className="text-2xl text-gray-600 group-hover:text-white" />
-              <h3 className="text-base text-gray-800 group-hover:text-white font-semibold">
-                Notifications
-              </h3>
+            <div className="my-4 border-b border-gray-100 pb-4 w-full">
+              <div className="flex mb-2 justify-start items-center gap-4 pl-5 p-2 rounded-md group cursor-pointer hover:bg-[#DC6F6F] hover:text-white transition-colors duration-300">
+                <MdOutlineSettings className="text-2xl text-gray-600 group-hover:text-white" />
+                <h3 className="text-base text-gray-800 group-hover:text-white font-semibold">
+                  Settings
+                </h3>
+              </div>
+              <div className="flex mb-2 justify-start items-center gap-4 pl-5 p-2 rounded-md group cursor-pointer hover:bg-[#DC6F6F] hover:text-white transition-colors duration-300">
+                <MdDelete className="text-2xl text-gray-600 group-hover:text-white" />
+                <h3 className="text-base text-gray-800 group-hover:text-white font-semibold">
+                  Delete Account
+                </h3>
+              </div>
             </div>
-            <div className="flex mb-2 justify-start items-center gap-4 pl-5 p-2 rounded-md group cursor-pointer hover:bg-[#DC6F6F] hover:text-white transition-colors duration-300">
-              <FaRegComments className="text-2xl text-gray-600 group-hover:text-white" />
-              <h3 className="text-base text-gray-800 group-hover:text-white font-semibold">
-                Discussion
-              </h3>
-            </div>
-            <div className="flex mb-2 justify-start items-center gap-4 pl-5 p-2 rounded-md group cursor-pointer hover:bg-[#DC6F6F] hover:text-white transition-colors duration-300">
-              <CgHeart className="text-2xl text-gray-600 group-hover:text-white" />
-              <h3 className="text-base text-gray-800 group-hover:text-white font-semibold">
-                Favourites
-              </h3>
-            </div>
-            <div className="flex mb-2 justify-start items-center gap-4 pl-5 p-2 rounded-md group cursor-pointer hover:bg-[#DC6F6F] hover:text-white transition-colors duration-300">
-              <AiOutlineQuestionCircle className="text-2xl text-gray-600 group-hover:text-white" />
-              <h3 className="text-base text-gray-800 group-hover:text-white font-semibold">
-                Help and Support
-              </h3>
-            </div>
-           
-          </div>
-          <div className="my-4 border-b border-gray-100 pb-4 w-full">
-            <div className="flex mb-2 justify-start items-center gap-4 pl-5 p-2 rounded-md group cursor-pointer hover:bg-[#DC6F6F] hover:text-white transition-colors duration-300">
-              <MdOutlineSettings className="text-2xl text-gray-600 group-hover:text-white" />
-              <h3 className="text-base text-gray-800 group-hover:text-white font-semibold">
-                Settings
-              </h3>
-            </div>
-            <div className="flex mb-2 justify-start items-center gap-4 pl-5 p-2 rounded-md group cursor-pointer hover:bg-[#DC6F6F] hover:text-white transition-colors duration-300">
-              <MdDelete className="text-2xl text-gray-600 group-hover:text-white" />
-              <h3 className="text-base text-gray-800 group-hover:text-white font-semibold">
-                Delete Account
-              </h3>
-            </div>
-          </div>
-          <div className="my-4 border-b border-gray-100 pb-4 w-full">
-            <div className="flex mb-2 justify-start items-center gap-4 pl-5 p-2 rounded-md group cursor-pointer hover:bg-[#DC6F6F] hover:text-white transition-colors duration-300">
-              <MdOutlineLogout className="text-2xl text-gray-600 group-hover:text-white" />
-              <h3 className="text-base text-gray-800 group-hover:text-white font-semibold">
-                Logout
-              </h3>
+            <div className="my-4 border-b border-gray-100 pb-4 w-full">
+              <div className="flex mb-2 justify-start items-center gap-4 pl-5 p-2 rounded-md group cursor-pointer hover:bg-[#DC6F6F] hover:text-white transition-colors duration-300">
+                <MdOutlineLogout className="text-2xl text-gray-600 group-hover:text-white" />
+                <h3 className="text-base text-gray-800 group-hover:text-white font-semibold">
+                  Logout
+                </h3>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
-
-export default Sidebar;
