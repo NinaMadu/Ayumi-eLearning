16be393e64ae0098b9ffc55fac8735182f321f66
NinaMadu <136308860+NinaMadu@users.noreply.github.com>
2024-07-29 21:26:13 +0530

    login added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Sidemenu from './components/Sidemenu';
 import Home from './pages/Home';
 import About from './pages/About';
 import SignUp from './pages/Signup'
+import Signin from './pages/Signin';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Signin />} />
       </Routes>
       
     </BrowserRouter>
