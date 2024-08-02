22b5ecc79c6eafd269ec36f8183fad506acb6ff2
kasunDilumina <kasundilumina2016@gmail.com>
2024-08-02 23:47:16 +0530

    Instructor and course models
    created Instructor and course models

---
+++ backend/models/course.model.js
@@ -0,0 +1,65 @@
+import mongoose from 'mongoose';
+
+const courseSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        enum:[], // should be  added
+        required: true,
+    },
+    difficultyLevel: {
+        type: String,
+        enum:["Beginner", "Intermediate", "Advanced"], //may be changed
+        required: true,
+    },
+    prerequisites: {
+        type: [String],
+        required: true,
+    },
+    courseObjectives: {
+        type: [String],
+        required: true,
+    },
+    duration: {
+        type: String,
+        required: true,
+    },
+    enrollmentOptions: {
+        //may be changed
+        type: String,
+        required: true,
+    },
+    Pricing: {
+        type: number,
+        required: true,
+    },
+    courseVisibility:{
+        type: Boolean,  //true or false
+        required: true,
+    },
+    courseMaterials:{
+        type: [String],
+
+    },
+    videos:{
+        type: [String],
+    },
+
+    instructor:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Instructor',
+    },
+    
+
+},{timestamps: true});
+
+const Course = mongoose.model('Course', courseSchema);
+
+export default Course;
