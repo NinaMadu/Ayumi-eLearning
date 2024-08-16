3b13646e4abb99bf5cdef373798a2311679b403d
NinaMadu <136308860+NinaMadu@users.noreply.github.com>
2024-08-16 07:59:09 +0530

    user page layout

--- frontend/src/pages/User/Dashboard.jsx
+++ frontend/src/pages/User/Dashboard.jsx
@@ -1,18 +1,12 @@
 import React from 'react'
-import Sidemenu from '../../components/Sidemenu';
-import Header from '../../components/Header';
+import UserLayout from '../../components/UserLayout';
 
 export const UDashboard = () => {
   return (
-    <>
-        {/* Header Component */}
-        <Header />
-
-        {/* Layout with Sidemenu and main content */}
-        <div className="flex pt-20">  {/* pt-20 to adjust for the fixed header height */}
-            <Sidemenu />
-            
+    <UserLayout>
+        <div>
+            <h1>User Dashboard</h1>
         </div>
-    </>
+    </UserLayout>
 );
 }
