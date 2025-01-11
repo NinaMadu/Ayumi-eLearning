677d5c82f169dbe77d498cdc4f113910611b16f9
Imalsha-Ranasinghe <imalsharanasinghe2020@gmail.com>
2025-01-11 10:38:26 +0530

    filter enhancement

--- frontend/src/pages/Instructor/Dashboard.jsx
+++ frontend/src/pages/Instructor/Dashboard.jsx
@@ -14,16 +14,18 @@ function Dashboard() {
   useEffect(() => {
     const fetchUserCount = async () => {
       try {
-        const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/users/count`);
+        const response = await fetch(
+          `${import.meta.env.VITE_API_BASE_URL}/api/users/count`
+        );
         const data = await response.json();
 
         if (response.ok) {
-          setUserCount(data.userCount); 
+          setUserCount(data.userCount);
         } else {
-          setError(data.message || 'Failed to fetch user count');
+          setError(data.message || "Failed to fetch user count");
         }
       } catch (err) {
-        setError('Error fetching user count');
+        setError("Error fetching user count");
       } finally {
         setLoading(false);
       }
@@ -34,23 +36,24 @@ function Dashboard() {
   useEffect(() => {
     const fetchCourseCount = async () => {
       try {
-        const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/course/count`);
+        const response = await fetch(
+          `${import.meta.env.VITE_API_BASE_URL}/api/course/count`
+        );
         const data = await response.json();
 
         if (response.ok) {
-          setCourseCount(data.courseCount); 
+          setCourseCount(data.courseCount);
         } else {
-          setError(data.message || 'Failed to fetch course count');
+          setError(data.message || "Failed to fetch course count");
         }
       } catch (err) {
-        setError('Error fetching course count');
+        setError("Error fetching course count");
       } finally {
         setLoading(false);
       }
     };
     fetchCourseCount();
   }, []);
-  
 
   if (loading) {
     return <p>Loading...</p>;
@@ -62,32 +65,37 @@ function Dashboard() {
 
   return (
     <AdminLayout>
-      <div className="min-h-screen bg-gray-100 p-8">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-8">
+      <div className="min-h-screen bg-gray-50 p-8 shadow-lg rounded-lg">
+        <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-3 gap-4 mb-8">
           <div className="bg-gradient-to-r bg-red-200 text-black p-4 rounded-lg shadow-md h-40">
             <div className="flex flex-col items-center justify-center h-full">
               <FaUsers className="mb-2 text-4xl sm:text-4xl" />
-              <div className="text-xl sm:text-2xl font-semibold">Total Users</div>
-              <div className="text-xl sm:text-2xl font-bold">{userCount}</div>
+              <div className="text-xl sm:text-xl font-semibold">
+                Total Users
+              </div>
+              <div className="text-xl sm:text-xl font-bold">{userCount}</div>
             </div>
-  
           </div>
           <div className="bg-gradient-to-r bg-blue-200 text-black p-4 rounded-lg shadow-md h-40">
             <div className="flex flex-col items-center justify-center h-full">
-              <FaBook className="mb-2 text-2xl sm:text-3xl" />
-              <div className="text-xl sm:text-xl font-semibold">Total Courses</div>
-              <div className="text-xl sm:text-2xl font-bold">{courseCount}</div>
+              <FaBook className="mb-2 text-xl sm:text-3xl" />
+              <div className="text-xl sm:text-xl font-semibold">
+                Total Courses
+              </div>
+              <div className="text-xl sm:text-xl font-bold">{courseCount}</div>
             </div>
           </div>
           <div className="bg-gradient-to-r bg-slate-200 text-black p-4 rounded-lg shadow-md h-40">
             <div className="flex flex-col items-center justify-center h-full">
               <FaUserPlus className="mb-2 text-4xl sm:text-4xl" />
-              <div className="text-xl sm:text-xl font-semibold">New Enrollments</div>
-              <div className="text-xl sm:text-2xl font-bold">0</div>
+              <div className="text-xl sm:text-lg font-semibold">
+                New Enrollments
+              </div>
+              <div className="text-xl sm:text-xl font-bold">0</div>
             </div>
           </div>
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-4 mb-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-8">
           <Link to={"/instructor/user-management"} className="flex">
             <button className="w-full h-full bg-gradient-to-r bg-cyan-800 text-white font-semibold p-3 sm:p-4 rounded-lg shadow-md hover:bg-cyan-600 transition min-h-[80px]">
               Manage Users
